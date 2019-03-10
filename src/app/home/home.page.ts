c65c797f89ec097f9b324d7bd858fe9f857e582a
skouro <restrepomesadiego@gmail.com>
2019-03-10 14:18:42 -0500

    se inicio form

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Validators, FormBuilder, FormGroup, FormControl} from '@angular/forms';
+
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+    constructor(public formBuilder: FormBuilder) {
+    }
 
+    formLogin: FormControl;
+
+    onSubmit(values) {
+        console.log(values);
+    }
 }
