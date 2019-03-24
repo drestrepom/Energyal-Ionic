60993c369061686c07d995e5f883b1fd5427bf36
skouro <restrepomesadiego@gmail.com>
2019-03-24 15:19:08 -0500

    comienzo de tabs

--- src/app/components/index/index.component.ts
+++ src/app/components/index/index.component.ts
@@ -4,120 +4,131 @@ import {BaseChartDirective, Color, Label} from 'ng2-charts';
 
 // @ts-ignore
 import * as pluginAnnotations from 'chartjs-plugin-annotation';
+import {SocketService} from '../../services/socket.service';
+import {Alerts} from '../../../utils/alerts';
+import {EventListener} from '@angular/core/src/debug/debug_node';
 
 @Component({
-  selector: 'app-index',
-  templateUrl: './index.component.html',
-  styleUrls: ['./index.component.scss'],
+    selector: 'app-index',
+    templateUrl: './index.component.html',
+    styleUrls: ['./index.component.scss'],
 })
 export class IndexComponent implements OnInit {
+// @ts-ignore
+    constructor(private  socketService: SocketService, private alerts: Alerts) {
+        this.socketService.myEmitter.subscribe(value => {
+            console.log(value);
+            this.data.shift();
+            this.data.push(value);
+            alerts.presentToast(value);
+            console.log(this.data);
+        });
+    }
 
-   data = [65, 59, 80, 81, 56, 55, 40];
-  public lineChartData: ChartDataSets[] = [
-    { data: this.data, label: 'Series A' },
+    data = [65, 59, 80, 81, 56, 55, 40];
+    public lineChartData: ChartDataSets[] = [
+        {data: this.data, label: 'Series A'},
 //     data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
 // { data: [180, 480, 770, 90, 1000, 270, 400], label: 'Series C', yAxisID: 'y-axis-1' }    {
-  ];
-  public lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-  public lineChartOptions: (ChartOptions & { annotation: any }) = {
-    responsive: true,
-    scales: {
-      // We use this empty structure as a placeholder for dynamic theming.
-      xAxes: [{}],
-      yAxes: [
-        {
-          id: 'y-axis-0',
-          position: 'left',
+    ];
+    public lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+    public lineChartOptions: (ChartOptions & { annotation: any }) = {
+        responsive: true,
+        scales: {
+            // We use this empty structure as a placeholder for dynamic theming.
+            xAxes: [{}],
+            yAxes: [
+                {
+                    id: 'y-axis-0',
+                    position: 'left',
+                },
+                {
+                    id: 'y-axis-1',
+                    position: 'right',
+                    gridLines: {
+                        color: 'rgba(255,0,0,0.3)',
+                    },
+                    ticks: {
+                        fontColor: 'red',
+                    }
+                }
+            ]
         },
-        {
-          id: 'y-axis-1',
-          position: 'right',
-          gridLines: {
-            color: 'rgba(255,0,0,0.3)',
-          },
-          ticks: {
-            fontColor: 'red',
-          }
+        annotation: {
+            annotations: [
+                {
+                    type: 'line',
+                    mode: 'vertical',
+                    scaleID: 'x-axis-0',
+                    value: 'March',
+                    borderColor: 'orange',
+                    borderWidth: 2,
+                    label: {
+                        enabled: true,
+                        fontColor: 'orange',
+                        content: 'LineAnno'
+                    }
+                },
+            ],
+        },
+    };
+    public lineChartColors: Color[] = [
+        { // grey
+            backgroundColor: 'rgba(148,159,177,0.2)',
+            borderColor: 'rgba(148,159,177,1)',
+            pointBackgroundColor: 'rgba(148,159,177,1)',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+        },
+        { // dark grey
+            backgroundColor: 'rgba(77,83,96,0.2)',
+            borderColor: 'rgba(77,83,96,1)',
+            pointBackgroundColor: 'rgba(77,83,96,1)',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgba(77,83,96,1)'
+        },
+        { // red
+            backgroundColor: 'rgba(255,0,0,0.3)',
+            borderColor: 'red',
+            pointBackgroundColor: 'rgba(148,159,177,1)',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgba(148,159,177,0.8)'
         }
-      ]
-    },
-    annotation: {
-      annotations: [
-        {
-          type: 'line',
-          mode: 'vertical',
-          scaleID: 'x-axis-0',
-          value: 'March',
-          borderColor: 'orange',
-          borderWidth: 2,
-          label: {
-            enabled: true,
-            fontColor: 'orange',
-            content: 'LineAnno'
-          }
-        },
-      ],
-    },
-  };
-  public lineChartColors: Color[] = [
-    { // grey
-      backgroundColor: 'rgba(148,159,177,0.2)',
-      borderColor: 'rgba(148,159,177,1)',
-      pointBackgroundColor: 'rgba(148,159,177,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-    },
-    { // dark grey
-      backgroundColor: 'rgba(77,83,96,0.2)',
-      borderColor: 'rgba(77,83,96,1)',
-      pointBackgroundColor: 'rgba(77,83,96,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(77,83,96,1)'
-    },
-    { // red
-      backgroundColor: 'rgba(255,0,0,0.3)',
-      borderColor: 'red',
-      pointBackgroundColor: 'rgba(148,159,177,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    ];
+    public lineChartLegend = true;
+    public lineChartType = 'line';
+    public lineChartPlugins = [pluginAnnotations];
+
+    @ViewChild(BaseChartDirective) chart: BaseChartDirective;
+
+    ngOnInit() {
     }
-  ];
-  public lineChartLegend = true;
-  public lineChartType = 'line';
-  public lineChartPlugins = [pluginAnnotations];
 
-  @ViewChild(BaseChartDirective) chart: BaseChartDirective;
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-  public randomize(): void {
-    const lineChartData: ChartDataSets[] = new Array(this.lineChartData.length);
-    for (let i = 0; i < this.lineChartData.length; i++) {
-      lineChartData[i] = { data: new Array(this.lineChartData[i].data.length), label: this.lineChartData[i].label };
-      for (let j = 0; j < this.lineChartData[i].data.length; j++) {
-        lineChartData[i].data[j] = Math.floor((Math.random() * (i < 2 ? 100 : 1000)) + 1);
-      }
+    public randomize(): void {
+        const lineChartData: ChartDataSets[] = new Array(this.lineChartData.length);
+        for (let i = 0; i < this.lineChartData.length; i++) {
+            lineChartData[i] = {data: new Array(this.lineChartData[i].data.length), label: this.lineChartData[i].label};
+            for (let j = 0; j < this.lineChartData[i].data.length; j++) {
+                lineChartData[i].data[j] = Math.floor((Math.random() * (i < 2 ? 100 : 1000)) + 1);
+            }
+        }
+        this.lineChartData = lineChartData;
     }
-    this.lineChartData = lineChartData;
-  }
 
-  // events
-  public chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
-    console.log(event, active);
-  }
+    // events
+    public chartClicked({event, active}: { event: MouseEvent, active: {}[] }): void {
+        console.log(event, active);
+    }
 
-  public chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
-    console.log(event, active);
-  }
+    public chartHovered({event, active}: { event: MouseEvent, active: {}[] }): void {
+        console.log(event, active);
+    }
 
-  public hideOne() {
-    const isHidden = this.chart.isDatasetHidden(1);
-    this.chart.hideDataset(1, !isHidden);
-  }
+    public hideOne() {
+        const isHidden = this.chart.isDatasetHidden(1);
+        this.chart.hideDataset(1, !isHidden);
+    }
 }
---
+++ src/config/config.ts
@@ -0,0 +1,2 @@
+// export  const URL_API = 'http://10.12.19.88:3001/';
+export  const URL_API = 'http://192.168.0.101:3000/';
