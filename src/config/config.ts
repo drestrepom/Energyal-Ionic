e9f32e0b905ec58d70b86e737a5fe58a413cd5ac
Daniel <darkhell2241@gmail.com>
2019-04-08 06:43:18 -0500

    tabs modificacion

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,3 @@
-// export  const URL_API = 'http://192.168.137.1:3000/';
-export  const URL_API = 'http://192.168.1.19:3000/';
+export  const URL_API = 'http://192.168.137.1:3000/';
+// export  const URL_API = 'http://192.168.1.19:3000/';
 // export  const URL_API = 'https://energyal.herokuapp.com/';
