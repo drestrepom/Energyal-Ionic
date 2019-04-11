7636eaef25b8dda208798343e53cafd5766628b9
Daniel <darkhell2241@gmail.com>
2019-04-10 20:02:19 -0500

    tabs que yo hice pero melo hace lo que le da la gana

---
+++ src/app/pages/index/r-time/r-time.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-r-time',
+  templateUrl: './r-time.component.html',
+  styleUrls: ['./r-time.component.scss'],
+})
+export class RTimeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
